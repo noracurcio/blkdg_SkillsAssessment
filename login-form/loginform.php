319396c41caf5253070a8a137bf84cb08e21be7a
noracurcio <noracurcio@gmail.com>
2021-09-08 16:54:38 -0600

    restarting with connection:

--- login-form/loginform.php
+++ login-form/loginform.php
@@ -24,52 +24,61 @@ get_header();
 </form>
 
 
-<?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
-$db_db = 'information_schema';
 
-$mysqli = @new mysqli(
-    $db_host,
-    $db_user,
-    $db_password,
-    $db_db
-);
-
-if ($mysqli->connect_error) {
-    echo 'Errno: ' . $mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: ' . $mysqli->connect_error;
-    exit();
-}
-
-echo 'Success: A proper connection to MySQL was made.';
-echo '<br>';
-echo 'Host information: ' . $mysqli->host_info;
-echo '<br>';
-echo 'Protocol version: ' . $mysqli->protocol_version;
-
-$sql = "INSERT INTO form_data (id, userFirstName, userLastName, userUsername, userPassword)
-VALUES (NULL, $firstname, $lastname, $username, $hashpass)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$mysqli->close();
-?>
 <?php
 
-$sql = "SELECT * FROM `form_data`";
+$conn = mysqli_connect('localhost', 'root', 'root', 'nora_db');
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+// connection to mysql db
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_password = 'root';
+// $db_db = 'information_schema';
+
+// $mysqli = @new mysqli(
+//     $db_host,
+//     $db_user,
+//     $db_password,
+//     $db_db
+// );
+
+// if ($mysqli->connect_error) {
+//     echo 'Errno: ' . $mysqli->connect_errno;
+//     echo '<br>';
+//     echo 'Error: ' . $mysqli->connect_error;
+//     exit();
+// }
+
+// echo 'Success: A proper connection to MySQL was made.';
+// echo '<br>';
+// echo 'Host information: ' . $mysqli->host_info;
+// echo '<br>';
+// echo 'Protocol version: ' . $mysqli->protocol_version;
+
+
+// // this SHOULD insert into db
+// $sql = "INSERT INTO form_data (id, userFirstName, userLastName, userUsername, userPassword)
+// VALUES (1, $firstname, $lastname, $username, $hashpass)";
+
+// if ($conn->query($sql) === TRUE) {
+//     echo "New record created successfully";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// $mysqli->close();
+
+
 
 
 
 
 // print results to an array
 print_r($_POST);
+
+
 
 
 
@@ -98,6 +107,9 @@ if (isset($_POST['submit'])) {
 
 // this exports the variables needed in next function
 var_dump($firstname, $lastname, $username, $hashpass);
+
+
+
 
 // function myFunction($firstname, $lastname, $hashpass, $username)
 // {
