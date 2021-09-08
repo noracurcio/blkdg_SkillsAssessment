86e4da6f40fecd93bd2eb87274386371ba81af95
noracurcio <noracurcio@gmail.com>
2021-09-08 11:43:20 -0600

    input form outline.

--- login-form/loginform.php
+++ login-form/loginform.php
@@ -13,6 +13,12 @@ get_header();
     <label for="user-lastname">
         <input id="user-lastname" type="text" name="user-lastname" value="">
     </label>
+    <label for="user-username">
+        <input id="user-username" type="text" name="user-username" value="">
+    </label>
+    <label for="user-dob">
+        <input id="user-dob" type="text" name="user-dob" value="">
+    </label>
 </form>
 <?php
 get_footer()
