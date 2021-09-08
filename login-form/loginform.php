6a0504fdf2d2470cbe0c8f14e196ca169b3e4d5e
noracurcio <noracurcio@gmail.com>
2021-09-07 18:42:41 -0600

    Adding input field for username.

--- login-form/loginform.php
+++ login-form/loginform.php
@@ -7,6 +7,9 @@
 get_header();
 ?>
 <form action="" method="post">
+    <label for="user-username">username
+        <input id="user-username" type="text" name="user-hobby" value="">
+    </label>
 </form>
 <?php
 get_footer()
