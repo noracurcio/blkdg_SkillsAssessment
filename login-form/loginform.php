2d1f6abb536e2f53a4446e503bfb9af933b38bde
noracurcio <noracurcio@gmail.com>
2021-09-09 16:48:06 -0600

    added alert statement removed reroute

--- login-form/loginform.php
+++ login-form/loginform.php
@@ -38,10 +38,14 @@ get_header();
                 <label style="color:#01273C;" for="user-password"> Password:
                     <input style="background-color:#A4B2BB" type="text" name="userPassword" id="user-password">
                 </label>
+                <br><br>
+                <label style="color:#01273C;" for="pass-check"> Re Enter Password:
+                    <input style="background-color:#A4B2BB" type="text" name="pass-check" id="pass-check">
+                </label>
             </p>
             <br><br><br>
             <p>
-                <input style="color:#BCC8D4;" type="submit" name="submit" value="Submit">
+                <input onclick="<?php header("location: /submit"); ?>" style="color:#BCC8D4;" type="submit" name="submit" value="Submit">
             </p>
 
         </form>
@@ -57,11 +61,17 @@ get_header();
 <?php
 
 
+
 //capturing variables from the form 
 $firstName =  $_REQUEST['firstName'];
 $lastName = $_REQUEST['lastName'];
 $userName = $_REQUEST['userName'];
 $password = $_REQUEST['userPassword'];
+$passCheck = $_REQUEST['passCheck'];
+
+
+
+
 //hashes password into 32 characters
 $hashPass = md5($password);
 
@@ -74,8 +84,9 @@ if (isset($_POST['submit'])) {
     $lastname = (!empty($_POST['user-lastname'])) ? sanitize_text_field($_POST['user-lastname']) : '';
     $username = (!empty($_POST['user-username'])) ? sanitize_text_field($_POST['user-username']) : '';
     $password = (!empty($_POST['user-password'])) ? sanitize_text_field($_POST['user-password']) : '';
-}
 
+    echo '<script>alert("Thank you for creating an account!")</script>';
+}
 
 
 
@@ -97,7 +108,9 @@ if ($conn === false) {
 // mysql guery to insert into table - added ID
 $sql = "INSERT INTO form_data  VALUES ('$firstName', 
             '$lastName', '$userName' ,'$hashPass', 0)";
-// $squ = "UPDATE form_data ('$firstName', $lastName, '$userName', '$hashPass')
+
+//Commented for now - trying to find and replace any data entries where the username matches
+// $sql = "UPDATE form_data ('$firstName', $lastName, '$userName', '$hashPass')
 // WHERE userName = $userName";
 
 
