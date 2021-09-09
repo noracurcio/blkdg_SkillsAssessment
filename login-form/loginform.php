7383cefa0cc247a157f8dc924a85ef9647010fe8
noracurcio <noracurcio@gmail.com>
2021-09-08 18:54:42 -0600

    trying to capture variables.

--- login-form/loginform.php
+++ login-form/loginform.php
@@ -6,19 +6,20 @@
 
 get_header();
 ?>
+<h1>Please Enter Your Log In Credentials</h1>
 <form action="" method="post">
     <label for="user-firstname"> First Name
-        <input id="user-firstname" type="text" name="firstName" id="firstName">
+        <input type="text" name="firstName" id="user-firstname">
     </label>
     <label for="user-lastname"> Last Name
-        <input id="user-lastname" type="text" name="lastName" id="lastName">
+        <input type="text" name="lastName" id="user-lastname">
     </label>
     <label for="user-username"> Username
-        <input id="user-username" type="text" name="userName" id="userName">
+        <input type="text" name="userName" id="user-username">
     </label>
 
     <label for="user-password"> Password
-        <input id="user-password" type="text" name="userPassword" id="userPassword">
+        <input type="text" name="userPassword" id="user-password">
     </label>
     <input type="submit" name="submit" value="Submit">
 </form>
@@ -31,45 +32,48 @@ get_header();
 
 
 
+$firstName =  $_REQUEST['firstName'];
+$lastName = $_REQUEST['lastName'];
+$userName = $_REQUEST['userName'];
+$password = $_REQUEST['userPassword'];
+
+
+
+
+// // print results to an array
+// print_r($_POST);
 
 
 
 
 
-// print results to an array
-print_r($_POST);
+
+
+// // if statement - isset is an inherant action - if true POST data stored in submit
+// if (isset($_POST['submit'])) {
+//     // echo 'test';
+//     // checking if its NOT(!) empty then post the value. Store the value of firstname into the $firstname variable. 
+//     // sanitize_text_field cleans inout field before we feed anything into the DB
+//     $firstname = (!empty($_POST['user-firstname'])) ? sanitize_text_field($_POST['user-firstname']) : '';
+//     $lastname = (!empty($_POST['user-lastname'])) ? sanitize_text_field($_POST['user-lastname']) : '';
+//     $username = (!empty($_POST['user-username'])) ? sanitize_text_field($_POST['user-username']) : '';
+//     $password = (!empty($_POST['user-password'])) ? sanitize_text_field($_POST['user-password']) : '';
 
 
 
 
+//     echo "First Name: $firstname";
+//     echo "Last Name: $lastname";
+//     echo "Username: $username";
+//     echo "Password: $password";
+//     // Hashing password and storing in hashpass variable
+//     $hashpass = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
 
+//     echo $hashpass;
+// }
 
-
-// if statement - isset is an inherant action - if true POST data stored in submit
-if (isset($_POST['submit'])) {
-    // echo 'test';
-    // checking if its NOT(!) empty then post the value. Store the value of firstname into the $firstname variable. 
-    // sanitize_text_field cleans inout field before we feed anything into the DB
-    $firstname = (!empty($_POST['user-firstname'])) ? sanitize_text_field($_POST['user-firstname']) : '';
-    $lastname = (!empty($_POST['user-lastname'])) ? sanitize_text_field($_POST['user-lastname']) : '';
-    $username = (!empty($_POST['user-username'])) ? sanitize_text_field($_POST['user-username']) : '';
-    $password = (!empty($_POST['user-password'])) ? sanitize_text_field($_POST['user-password']) : '';
-
-
-
-
-    echo "First Name: $firstname";
-    echo "Last Name: $lastname";
-    echo "Username: $username";
-    echo "Password: $password";
-    // Hashing password and storing in hashpass variable
-    $hashpass = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
-
-    echo $hashpass;
-}
-
-// this exports the variables needed in next function
-var_dump($firstname, $lastname, $username, $password);
+// // this exports the variables needed in next function
+// var_dump($firstname, $lastname, $username, $password);
 
 
 // connect to database
@@ -85,7 +89,7 @@ if ($conn === false) {
 
 
 // mysql guery to insert into table
-$sql = "INSERT INTO form_data  VALUES ('test', 
+$sql = "INSERT INTO form_data  VALUES ($first_name, 
             'test','test','test')";
 
 if (mysqli_query($conn, $sql)) {
